3e7eeb3b5d27fdcbb98a9e9b27582559e8b4e177
Scyruz <49880669+Scyruz@users.noreply.github.com>
2021-02-16 11:28:04 -0700

    Add files via upload

---
+++ HelloTec/HelloTec/HelloTec/ViewController.swift
@@ -0,0 +1,27 @@
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func showMessage(_ sender: UIButton) {
+        var emojiDict = ["🍒":"Cherry", "🌞":"Sun", "🏄🏼‍♀️":"Surfer"]
+                
+                let selectedButton = sender
+                
+                if let wordToLookup = selectedButton.titleLabel?.text {
+                    
+                    let meaning = emojiDict[wordToLookup]
+                    
+                    let alertController = UIAlertController(title: "Meaning", message: "Hello " + meaning!, preferredStyle: UIAlertController.Style.alert)
+                    
+                    alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+                    present(alertController, animated: true, completion: nil)
+                }
+    }
+    
+}
+
